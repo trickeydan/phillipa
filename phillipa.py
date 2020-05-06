97a5f1c9f63c988ced1ae94fc49cd0114a624a46
Dan Trickey <dan@trickey.io>
2020-05-06 20:27:26 +0100

    Logging

--- phillipa.py
+++ phillipa.py
@@ -1,10 +1,16 @@
 import discord
+import logging
 
-GOOD_KEYWORDS = ["dolphin", "phil", "phillipa", "rishi", "jesters", "bills bills bills", "the crown inn", "southampton", "soton"]
-BAD_KEYWORDS = ["portsmouth"]
+logging.basicConfig(level=logging.INFO)
+
+GOOD_KEYWORDS = ["dolphin", "phil", "phillipa", "rishi", "jesters", "bills bills bills", "the crown inn", "southampton", "soton", "flower"]
+BAD_KEYWORDS = ["portsmouth", "fishing net"]
 
 
 class PhillipaBot(discord.Client):
+
+    async def on_ready(self):
+        logging.info(f"Connected as {self.user}")
 
     async def on_message(self, message):
         
@@ -23,15 +29,19 @@ class PhillipaBot(discord.Client):
             bad_conditions.append(word in content)
 
         if any(good_conditions) and not any(bad_conditions):
+            logging.info("I like")
             await message.add_reaction("💮")
 
         if any(bad_conditions):
+            logging.info("I dislike")
             await message.add_reaction("😠")
 
     async def on_reaction_add(self, reaction, user):
         if not user.bot and reaction.emoji == "💮":
+            logging.info(f"{user} is nice.")
             await user.send("💮")
 
-client = PhillipaBot()
-
-client.run("NzA3NjYyNjEwMTk5MzQ3Mjgx.XrMFiQ.BsSGHNDGhlAQzsjesBZhQKxEnV0")
+if __name__ == "__main__":
+    logging.info("Starting Phillipa")
+    client = PhillipaBot()
+    client.run("NzA3NjYyNjEwMTk5MzQ3Mjgx.XrMFiQ.BsSGHNDGhlAQzsjesBZhQKxEnV0")
